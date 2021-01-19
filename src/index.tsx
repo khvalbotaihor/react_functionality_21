6f591bf179b8dc44c4f2b87f65d2fb5d434d9ec1
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-01-19 14:38:02 +0200

    1 - code investigation

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,76 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <div>hello</div>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+let a = {
+    name: "Dimich",
+    age: 110,
+    address: {
+        city: "Minsk",
+        country: "Belarus"
+    }
+};
+
+
+let users = [
+    {
+        name: "Dimich",
+        age: 110,
+        address: {
+            city: "Minsk",
+            country: "Belarus"
+        }
+    },
+    {
+        name: "Victor",
+        age: 110,
+        address: {
+            city: "Minsk",
+            country: "Belarus"
+        }
+    }
+]
+
+console.log(users[0].name)
+
+
+
+let b : Array< number | string>
+b = [1,2,3,4,5]
+b = [1,2, "aaa", "fff"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
