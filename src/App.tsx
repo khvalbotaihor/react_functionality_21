84cf2b301bec45db758f8f7007c5985e9d3de56d
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-02-06 01:47:52 +0200

    2 - created accordion title and accordion body

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,24 @@ import React from 'react';
 import './App.css';
 
 function App() {
+    console.log("App rendering")
     return (
         <div className="App">
-            This is APP component
+            <AppTitle />
             <Rating />
             <Accordion />
+            <Rating />
         </div>
     );
 }
 
+function AppTitle() {
+    console.log("AppTitle rendering")
+    return <>This is APP component</>
+}
+
 function Rating() {
+    console.log("Rating rendering")
     return (
         <div>
             <Star />
@@ -24,20 +32,35 @@ function Rating() {
 }
 
 function Star() {
+    console.log("Star is rendering")
     return <div>start</div>
 }
 
 function Accordion() {
+    console.log("Accordion is rendering")
     return(<div>
-            <h3>Menu</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
+            <AccordionTitle />
+            <AccordionBody />
+
         </div>
 
     )
+}
+
+function AccordionTitle() {
+    console.log("Accordion titlr")
+    return(
+        <h3>Menu</h3>
+    )
+}
+
+function AccordionBody() {
+    console.log("Accordion body")
+    return  <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
 }
 
 export default App;
